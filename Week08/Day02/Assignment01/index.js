b3456a0285e7b7acd861628ff2bb73f21bf86b8b
somaiya <somaiyaqoriashi@gmail.com>
2026-03-04 22:54:42 +0430

    done

--- Week08/Day02/Assignment01/index.js
+++ Week08/Day02/Assignment01/index.js
@@ -2,3 +2,14 @@ function bmiCalc(w,h){
     bmi=w/(h*h);
     return bmi;
 }
+if (bmiCalc(w,h)<18.5){
+    console.log("Underweight");
+}else if (bmiCalc(w,h)>=18.5 && bmiCalc(w,h)<25){
+    console.log("Normal weight");
+}   else if (bmiCalc(w,h)>=25 && bmiCalc(w,h)<30){
+    console.log("Overweight");
+}else if (bmiCalc(w,h)>=30){
+    console.log("Obese");
+} else if(bmiCalc(w,h)>=35){
+    console.log("Extremely Obese");
+}
